3fa1fa91a408641ab138b01548d902f5c016a55e
shiliang <shiliang@crazymaplestudio.com>
2025-10-10 18:35:43 +0800

    feat： 仓库优化

--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -109,12 +109,11 @@ WSGI_APPLICATION = 'djangoblog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DJANGO_MYSQL_DATABASE') or 'djangoblog',
-        'USER': os.environ.get('DJANGO_MYSQL_USER') or 'root',
-        'PASSWORD': os.environ.get('DJANGO_MYSQL_PASSWORD') or 'root',
-        'HOST': os.environ.get('DJANGO_MYSQL_HOST') or '127.0.0.1',
-        'PORT': int(
-            os.environ.get('DJANGO_MYSQL_PORT') or 3306),
+        'NAME': 'djangoblog',
+        'USER': 'root',
+        'PASSWORD': 'bT990917',
+        'HOST': '127.0.0.1',
+        'PORT': int(3306),
         'OPTIONS': {
             'charset': 'utf8mb4'},
     }}
