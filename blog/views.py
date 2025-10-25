9057469e7599d78d553c312a7b721efcd11388b9
shiliang <1605559078@qq.com>
2025-10-25 21:51:25 +0800

    feat: fix

--- blog/views.py
+++ blog/views.py
@@ -38,7 +38,7 @@ class ArticleListView(ListView):
     link_type = LinkShowType.L
 
     def get_view_cache_key(self):
-        return self.request.get['pages']
+        return self.request.GET['pages']
 
     @property
     def page_number(self):
